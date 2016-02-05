cae74c41b8b98ca86193ae72e4ae76da8ce4d12e
Timo Rütten <timoruetten@Timos-MacBook-Pro.local>
2016-02-05 01:55:46 +0100

    Adding schemas with id to the scope to referencing them

--- jsonschema.js
+++ jsonschema.js
@@ -3,6 +3,12 @@ JsonSchema = class JsonSchema {
   constructor(schema, opt) {
     this.opt = _.extend({}, {}, opt);
     this.schema = schema;
+    /**
+      Adding the schema to the scope of schemas when it provides a id
+    */
+    if (schema.id) {
+      Validator.addSchema(schema, schema.id);
+    }
   }
 
   validate(doc) {
