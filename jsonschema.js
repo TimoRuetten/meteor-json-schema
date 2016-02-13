cd3e921fde6789f39d7b649649a5ffd15caa8de2
Timo Rütten <timoruetten@Timos-MacBook-Pro.local>
2016-02-13 13:43:26 +0100

    Preparing context of schema

--- jsonschema.js
+++ jsonschema.js
@@ -1,10 +1,11 @@
 
 JsonSchema = class JsonSchema {
   constructor(schema, opt) {
-
     this.opt = _.extend({}, {
       validator: 'default'
     }, opt);
+
+    this._contexts = {};
 
     this.Validator = JsonSchemaUtility.Validator(this.opt.validator);
 
@@ -52,6 +53,15 @@ JsonSchema = class JsonSchema {
     }, this.compositedSchema);
   }
 
+
+  context(key) {
+    key = key || '_default';
+    if (!this._contexts[key]) {
+      this._contexts[key] = new JsonSchemaContext(this);
+    }
+    return this._contexts[key];
+  }
+
   validate(doc) {
     return this.Validator.validate(doc, this.schema);
   }
@@ -62,5 +72,60 @@ JsonSchema = class JsonSchema {
       add reactive support
     */
     return this.getSchema(field).label || false;
+  }
+
+  getKeys() {
+    // this will return all possible keys like person, person.name, address, address.street, address.city...
+    return false;
+  }
+};
+
+
+JsonSchemaContext = class {
+  constructor(mjs) {
+    this._mjs = mjs;
+    // create a Dependency() for any change
+    this._depsAny = new Tracker.Dependency();
+
+    // Variables for error handling ?
+    this.invalidKeys = []; // all keys in dot notation which have problems: ['person.name', 'address']
+    this.validationErrors = []; // a cleaned version of errors
+    this._validationError = false; // original jsonschema error
+
+
+  }
+
+  isValid() {
+    this._depsAny.depend();
+    return null;
+  }
+  getErrors() {
+    // reactive
+    return null;
+  }
+  getErrorFor(field) {
+    return null;
+  }
+  isFieldValid(field) {
+    if (!this._depsField[field]) {
+      this._depsField[field] = new Tracker.Dependency();
+    }
+    this._depsField[field].depend();
+    return null;
+  }
+
+  validate(doc) {
+    // not reactive
+    const validation = this._mjs.validate(doc);
+    return null;
+  }
+  validateExisting() {
+    // like validate for update -> validats only given properties
+    return null;
+  }
+
+  getDoc() {
+    // returns the last validated doc
+    return null;
   }
 };
