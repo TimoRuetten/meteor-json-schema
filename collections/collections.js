92ec92a4c2e085d23b14ad4c90f62d7b7c752205
TimoRuetten <info@visu-dare.de>
2016-02-15 18:15:11 +0100

    Added misimultateCollectionUpdate with miniMongo

--- collections/collections.js
+++ collections/collections.js
@@ -1,7 +1,35 @@
+
+const _Collection = new Meteor.Collection(null);
+
+function simulateCollectionUpdate (modifier, opt, defaultDoc) {
+  if (!defaultDoc || !_.isObject(defaultDoc)) defaultDoc = {};
+
+  let _id = _Collection.insert(defaultDoc);
+  _Collection.update({
+    _id
+  }, modifier, opt);
+  let doc = _Collection.findOne(_id);
+  _Collection.remove({
+    _id
+  });
+  delete doc._id;
+  return doc;
+}
+
+/**
+  This will simulate a update on a collection based on the schema
+*/
+Mongo.Collection.prototype.simulateModifier = function (obj, opt, _id) {
+  if (!this._mjs || !(this._mjs instanceof JsonSchema)) {
+    throw new Meteor.Error(400, 'You are not able to simulate a modifier without attached a schema');
+  }
+  const defaultDoc = _id ? {}:{};
+  return simulateCollectionUpdate(obj, opt, defaultDoc);
+};
+
 /**
 * We want to add our schemas to our collections
 */
-
 
 Mongo.Collection.prototype.attachJsonSchema = function(schema, opt) {
   this._mjs = schema;
@@ -22,12 +50,16 @@ manipulateMongoMethods.map(function(method) {
         if (doc) {
           let validation = this._mjs.validate(doc);
           if (!validation.valid) {
-            // Trigger a error
+            /**
+            * TODO:
+            * Get the "new" JsonSchemaError object and cancel the method/threw the error
+            */
             throw new Meteor.Error(400, 'The Validation failed');
           }
         }
       } else if (method == 'update') {
-        // To do: Prepare a modifier object to a doc
+        const doc = args[1];
+
       }
     }
 
